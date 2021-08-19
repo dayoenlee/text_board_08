1075e977b07cb5ed839f57f37a6cafbf85b457cf
dayoenlee <dlekdus33483@gmail.com>
2021-08-19 11:48:13 +0900

    게시물 추사, 게시물 리스팅 기능 구현

--- src/com/sbs/example/textBorad/Main.java
+++ src/com/sbs/example/textBorad/Main.java
@@ -1,10 +1,24 @@
 package com.sbs.example.textBorad;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("안녕");
+		Scanner sc = new Scanner(System.in);
+		List<Article> articles= new ArrayList<>();
+		while(true) {
+			System.out.println("명령어 입력해주세요 :");
+			String command = sc.nextLine().trim();
+			if (command.equals("systam exit")) {
+				System.out.println("프로그램 종료");
+				break;
+			}else if (command.equals("")) {
+				
+			}
+				
+		}
 	}
-
 }
