bc81e06e0d6710c9e44239ef5ba09185bd0d98cb
dayoenlee <dlekdus33483@gmail.com>
2021-08-20 12:59:42 +0900

    게시물 Insert 적용

--- src/com/sbs/example/textBorad/Main.java
+++ src/com/sbs/example/textBorad/Main.java
@@ -1,5 +1,10 @@
 package com.sbs.example.textBorad;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
@@ -25,9 +30,55 @@ public class Main {
 				
 				Article article = new Article(id,title,body);
 				articles.add(article);
-				lastArticleId++;
+				// Insert적용
+				Connection conn = null;
+				PreparedStatement pstat = null;
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+					
+					
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title =\'"+ title +"\'";
+					sql += ",`body` =\'"+ body +"\';";
+					
+					pstat = conn.prepareStatement(sql);
+					int affectedRows = pstat.executeUpdate();
+					
+					System.out.println("affectedRows : " + affectedRows);
+					
+					
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstat != null && !conn.isClosed()) {
+							conn.close();
+						}
+					}catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 				
+				
+				lastArticleId++;
 				System.out.println(article);
+				//Insert 완료
+				
 			}else if(command.equals("article list")) {
 				System.out.println("== 게시글 리스트 ==");
 				if (articles.size() == 0) {
